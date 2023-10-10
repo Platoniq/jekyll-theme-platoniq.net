02dc87b4c855376260a981e261c7bec7213a063e
Alejandro Rueda <alejandro.rueda@nazaries.com>
2023-10-10 12:36:57 +0200

    search development

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,8 @@ $(document).ready(function () {
   SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
-    json: '/search.json'
+    json: '/search.json',
+    searchResultTemplate: '<li class="list-group-item"><a href="{url}" class="search-link">{title}</a></li>'
   })
 
 
